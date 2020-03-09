f3bf3f1104258a0372733db18068a52bb1c7f235
gofrendi <gofrendiasgard@gmail.com>
2020-03-09 17:19:56 +0700

    add redash docker file

---
+++ Makefile
@@ -0,0 +1,8 @@
+prepare:
+	docker-compose run --rm server create_db
+	docker-compose up -d
+
+stop:
+	docker stop ubhara_redis_1 ubhara_server_1 ubhara_scheduled_worker_1 ubhara_scheduler_1 ubhara_adhoc_worker_1 ubhara_nginx_1 ubhara_postgres_1
+clear:
+	docker rm ubhara_redis_1 ubhara_server_1 ubhara_scheduled_worker_1 ubhara_scheduler_1 ubhara_adhoc_worker_1 ubhara_nginx_1 ubhara_postgres_1
--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,7 @@
 import os
 import tweepy
-import pymysql.cursors
+import psycopg2
+import traceback
 
 twitter_access_token = os.environ['TWITTER_ACCESS_TOKEN']
 twitter_access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
@@ -8,10 +9,40 @@ twitter_access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 twitter_consumer_key = os.environ['TWITTER_CONSUMER_KEY']
 twitter_consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
 
-mysql_host = os.environ['MYSQL_HOST']
-mysql_user = os.environ['MYSQL_USER']
-mysql_password = os.environ['MYSQL_PASSWORD']
-mysql_db = os.environ['MYSQL_DB']
+postgres_host = 'localhost'
+postgres_database = 'postgres'
+postgres_user = 'postgresUser'
+postgres_password = 'postgresPassword'
+postgres_schema = 'tweet'
+
+postgres_create_db_sql = 'CREATE SCHEMA IF NOT EXISTS ' + \
+    postgres_schema + ';'
+postgres_create_table_sql = 'CREATE TABLE IF NOT EXISTS '+postgres_schema+'.statuses ( ' + \
+    '    id SERIAL PRIMARY KEY,' + \
+    '    tweet_id VARCHAR(50) NOT NULL,' + \
+    '    text TEXT,' + \
+    '    hashtag VARCHAR(50),' + \
+    '    user_id VARCHAR(50),' + \
+    '    user_name VARCHAR(100),' + \
+    '    retweet_count INTEGER,' + \
+    '    reply_count INTEGER,' + \
+    '    created_at VARCHAR(50)' + \
+    ')'
+
+
+def create_connection():
+    return psycopg2.connect(
+        host=postgres_host, user=postgres_user, password=postgres_password, dbname=postgres_database)
+
+
+def create_postgres_schema():
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(postgres_create_db_sql)
+    cursor.execute(postgres_create_table_sql)
+    cursor.close()
+    connection.commit()
+    connection.close()
 
 
 class MyStreamListener(tweepy.StreamListener):
@@ -21,58 +52,42 @@ class MyStreamListener(tweepy.StreamListener):
         self.connection = connection
 
     def on_status(self, status):
-        print('TEXT: ', status.text)
+        if status.retweeted or not hasattr(status, 'extended_tweet'):
+            return None
+        print('ID: ', status.id_str)
+        print('FULL_TEXT:', status.extended_tweet['full_text'])
         print('HASHTAGS: ', status.entities['hashtags'])
+        print('USER_ID:', status.user.id_str)
+        print('USER_NAME:', status.user.name)
+        print('RETWEET_COUNT: ', status.retweet_count)
+        print('REPLY_COUNT: ', status.reply_count)
         print('CREATED AT: ', status.created_at)
         print('======================================')
-        text = status.text
         hashtags = status.entities['hashtags']
-        created_at = status.created_at
         for hashtag in hashtags:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    'INSERT INTO statuses(text, hashtag, created_at) VALUES(%s, %s, %s)',
-                    (text, hashtag['text'], created_at))
+            hashtag_text = hashtag['text']
+            cursor = connection.cursor()
+            cursor.execute(
+                'INSERT INTO '+postgres_schema +
+                '.statuses(tweet_id, text, hashtag, user_id, user_name, retweet_count, reply_count, created_at) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)',
+                (status.id_str, status.extended_tweet['full_text'], hashtag_text, status.user.id_str, status.user.name, status.retweet_count, status.reply_count, status.created_at))
+            cursor.close()
             connection.commit()
 
 
-# create db connection
-connection = pymysql.connect(
-    host=mysql_host,
-    user=mysql_user,
-    password=mysql_password,
-    db=mysql_db,
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor)
+create_postgres_schema()
 
-try:
+connection = create_connection()
+twitter_auth = tweepy.OAuthHandler(
+    twitter_consumer_key, twitter_consumer_secret)
+twitter_auth.set_access_token(
+    twitter_access_token, twitter_access_token_secret)
+twitter_api = tweepy.API(twitter_auth)
 
-    # create table
-    with connection.cursor() as cursor:
-        cursor.execute(
-            '''
-            CREATE TABLE IF NOT EXISTS statuses( 
-                id int(11) NOT NULL AUTO_INCREMENT, 
-                text TEXT, 
-                hashtag VARCHAR(255), 
-                created_at VARCHAR(20),
-                PRIMARY KEY(id))
-            ''')
-    connection.commit()
-
-    # create twitter api
-    twitter_auth = tweepy.OAuthHandler(
-        twitter_consumer_key, twitter_consumer_secret)
-    twitter_auth.set_access_token(
-        twitter_access_token, twitter_access_token_secret)
-    twitter_api = tweepy.API(twitter_auth)
-
-    # create stream listener
-    myStreamListener = MyStreamListener(connection)
-    myStream = tweepy.Stream(auth=twitter_api.auth, listener=myStreamListener)
-    myStream.filter(
-        track=['#severeweather', '#corona', '#electionday'],
-        languages=['en'])
-
-finally:
-    connection.close()
+# create stream listener
+myStreamListener = MyStreamListener(connection)
+myStream = tweepy.Stream(
+    auth=twitter_api.auth, listener=myStreamListener, tweet_mode='extended')
+myStream.filter(
+    track=['a'],
+    languages=['en'])
